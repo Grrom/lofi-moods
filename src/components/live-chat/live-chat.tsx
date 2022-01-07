93acedab48e7ae3029dc3ff210520b11cd9043c1
Jerome <josejeromelalunio@gmail.com>
2022-01-07 11:28:45 +0800

    add chatbox

--- src/components/live-chat/live-chat.tsx
+++ src/components/live-chat/live-chat.tsx
@@ -1,5 +1,8 @@
+import { IconButton } from "../misc/icon-button/icon-button";
 import ChatBubble from "./chat-bubble";
 import "./live-chat.scss";
+
+import send from "../../assets/send.svg";
 
 export default function LiveChat() {
   return (
@@ -11,6 +14,14 @@ export default function LiveChat() {
           dateSent={new Date()}
           isVerified={true}
         />
+        <div className="chat-box-container">
+          <textarea className="chat-box" rows={1} />
+          <IconButton
+            icon={send}
+            isLoading={false}
+            onClick={() => console.log("send")}
+          />
+        </div>
       </div>
     </div>
   );
