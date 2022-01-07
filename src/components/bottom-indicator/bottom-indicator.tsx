fa34b5e0d803e77229e58e48760913921be79834
Jerome <josejeromelalunio@gmail.com>
2022-01-07 18:23:54 +0800

    add marquee when bottom message is overflowing

--- src/components/bottom-indicator/bottom-indicator.tsx
+++ src/components/bottom-indicator/bottom-indicator.tsx
@@ -1,16 +1,33 @@
+import { useLayoutEffect, useState } from "react";
+import { cursorTo } from "readline";
 import { useBottomMessage } from "../../global-state/bottom-message-provider";
 import { useBuffering } from "../../global-state/buffering-provider";
+import Helpers from "../../helpers/helpers";
 import { MiniLoader } from "../misc/loader/loader";
 import "./bottom-indicator.scss";
 
 export default function BottomIndicator() {
   const isBuffering = useBuffering();
   const message = useBottomMessage();
+  const [height, setHeight] = useState(0);
+
+  useLayoutEffect(() => {
+    function updateSize() {
+      setHeight(
+        Helpers.getById("bottom-indicator")?.getBoundingClientRect().height ?? 0
+      );
+    }
+    window.addEventListener("resize", updateSize);
+    if (isBuffering === false) updateSize();
+    return () => window.removeEventListener("resize", updateSize);
+  }, [isBuffering]);
 
   return (
     <span id="bottom-indicator">
-      {message}
-      {isBuffering && <MiniLoader />}
+      <span className={height > 36 ? "marquee" : ""}>
+        {message}
+        {isBuffering && <MiniLoader />}
+      </span>
     </span>
   );
 }
