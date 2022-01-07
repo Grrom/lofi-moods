707ab140b5f311a70b3ecb24a6e0957b26c1d8f6
Jerome <josejeromelalunio@gmail.com>
2022-01-07 16:08:06 +0800

    add backdrop filter

--- src/components/live-chat/live-chat.tsx
+++ src/components/live-chat/live-chat.tsx
@@ -8,12 +8,17 @@ export default function LiveChat() {
   return (
     <div id="live-chat-container">
       <div id="live-chat">
-        <ChatBubble
-          senderId="asdfdf"
-          message="the five boxing wizards jumped quickly, the five boxing wizards jumped quickly"
-          dateSent={new Date()}
-          isVerified={true}
-        />
+        <div className="chat-list-view">
+          {Array.from(Array(20).keys()).map((index) => (
+            <ChatBubble
+              key={index}
+              senderId="asdfdf"
+              message="the five boxing wizards jumped quickly, the five boxing wizards jumped quickly"
+              dateSent={new Date()}
+              isVerified={true}
+            />
+          ))}
+        </div>
         <div className="chat-box-container">
           <textarea className="chat-box" rows={1} />
           <IconButton
