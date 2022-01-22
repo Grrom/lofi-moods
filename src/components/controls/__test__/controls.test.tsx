f868307c571e0d5074abc58a04652da753ee49b9
Jerome <josejeromelalunio@gmail.com>
2022-01-22 16:47:21 +0800

    add basic render tests

---
+++ src/components/controls/__test__/controls.test.tsx
@@ -0,0 +1,6 @@
+import { render } from "@testing-library/react";
+import Controls from "../controls";
+
+test("Controls", () => {
+  render(<Controls />);
+});
---
+++ src/components/misc/loader/__test__/mini-loader.test.tsx
@@ -0,0 +1,6 @@
+import { render } from "@testing-library/react";
+import { MiniLoader } from "../loader";
+
+test("Mini Loader", () => {
+  render(<MiniLoader />);
+});
